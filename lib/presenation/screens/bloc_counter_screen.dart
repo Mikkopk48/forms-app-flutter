fc0ad36f4a2651fc4061c24397d1458038870d5e
Mikkopk48 <mikkopk48@gmail.com>
2025-05-13 11:55:53 -0300

    first commit

---
+++ lib/presenation/screens/bloc_counter_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bloc counter"),
+        actions: [
+          IconButton(icon: const Icon(Icons.refresh), onPressed: () {}),
+        ],
+      ),
+      body: const Center(child: Text("Counter value:")),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 1,
+            child: const Text('+3'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: 2,
+            child: const Text('+2'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: 3,
+            child: const Text('+1'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 15),
+        ],
+      ),
+    );
+  }
+}
