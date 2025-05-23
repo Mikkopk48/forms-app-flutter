bbf4829db95d0763909b45ff32afaacb8125c1a0
Mikko Piaggio <mikkopk48@gmail.com>
2025-05-23 10:47:52 -0300

    Refactor CounterBloc: update event handling and state initialization; add copyWith method to CounterState

--- lib/presenation/blocs/couter_bloc/counter_state.dart
+++ lib/presenation/blocs/couter_bloc/counter_state.dart
@@ -5,8 +5,11 @@ class CounterState extends Equatable {
   final int transactionCount;
   const CounterState({this.counter = 10, this.transactionCount = 0});
 
+  CounterState copyWith({int? counter, int? transactionCount}) => CounterState(
+    counter: counter ?? this.counter,
+    transactionCount: transactionCount ?? this.transactionCount,
+  );
+
   @override
   List<Object> get props => [counter, transactionCount];
 }
-
-final class CounterInitial extends CounterState {}
