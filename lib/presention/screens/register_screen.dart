21a9319ce9994e0b77955cf149088fd9afef76b1
Mikko Piaggio <mikkopk48@gmail.com>
2025-06-02 11:18:48 -0300

    Refactor registration screen: extract form into a separate widget and implement custom text form field with validation

--- lib/presention/screens/register_screen.dart
+++ lib/presention/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presention/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -17,25 +18,39 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            const FlutterLogo(size: 100),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            const SizedBox(height: 20),
-            FilledButton.tonalIcon(
-              onPressed: () {},
-              label: const Text('Guardar'),
-              icon: const Icon(Icons.save),
-            ),
-            const SizedBox(height: 20),
+            FlutterLogo(size: 100),
+            _RegisterForm(),
+            SizedBox(height: 20),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          const CustomTextFormField(),
+          const SizedBox(height: 20),
+          const CustomTextFormField(),
+          const SizedBox(height: 20),
+          FilledButton.tonalIcon(
+            onPressed: () {},
+            label: const Text('Guardar'),
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
     );
   }
