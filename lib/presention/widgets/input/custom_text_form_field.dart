b6b922ef267ce575a69bd86db9347e99d70e6e8c
Mikko Piaggio <mikkopk48@gmail.com>
2025-06-03 11:16:11 -0300

    Forms without state manager; Refactor registration form: convert to StatefulWidget, implement form validation, and enhance CustomTextFormField with dynamic properties

--- lib/presention/widgets/input/custom_text_form_field.dart
+++ lib/presention/widgets/input/custom_text_form_field.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  const CustomTextFormField({
+    super.key,
+    this.label,
+    this.hint,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+    this.obscureText = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +25,26 @@ class CustomTextFormField extends StatelessWidget {
       borderSide: BorderSide(color: colors.primary),
       borderRadius: BorderRadius.circular(40),
     );
-    return TextFormField(
-      onChanged: (value) {
-        print('Value $value');
-      },
-      validator: (value) {
-        if (value == null) return 'Campo es requerido';
-        if (value.isEmpty) return 'Campo es requerido';
-        if (value.trim().isEmpty) return 'Campo es requerido';
-        return null;
-      },
-      decoration: InputDecoration(
-        enabledBorder: border,
-        focusedBorder: border.copyWith(
-          borderSide: BorderSide(color: colors.primary),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextFormField(
+        onChanged: onChanged,
+        validator: validator,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: border,
+          focusedBorder: border.copyWith(
+            borderSide: BorderSide(color: colors.primary),
+          ),
+          isDense: true,
+          label: label != null ? Text(label!) : null,
+          hintText: hint,
+          suffixIcon: Icon(
+            Icons.supervised_user_circle_sharp,
+            color: colors.primary,
+          ),
+          errorText: errorMessage,
+          focusColor: colors.primary,
         ),
       ),
     );
